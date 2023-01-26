8c1c780e8468294f29ca3a6895e764df34c64cb5
Tristan Brandt <me@tristanbrandt.io>
2023-01-26 19:08:13 +0200

    add analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,18 @@
 import { type AppType } from "next/dist/shared/lib/utils";
 
+import { init } from "@socialgouv/matomo-next";
+
+const MATOMO_URL = process.env.NEXT_PUBLIC_MATOMO_URL || "";
+const MATOMO_SITE_ID = process.env.NEXT_PUBLIC_MATOMO_SITE_ID || "";
+
 import "../styles/globals.css";
+import { useEffect } from "react";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
+  useEffect(() => {
+    init({ url: MATOMO_URL, siteId: MATOMO_SITE_ID });
+  }, []);
+
   return <Component {...pageProps} />;
 };
 
