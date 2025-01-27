bbd6225b459ec59b0eacb55ef27905f3fb792ee1
Tristan Brandt <me@tristanbrandt.io>
2025-01-27 23:55:04 +0200

    add posthog

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,20 @@
-import { type AppType } from "next/dist/shared/lib/utils";
+import type { AppType } from "next/app";
+import posthog from "posthog-js";
+import { clientEnv } from "../env/schema.mjs";
 
-import "../styles/globals.css";
 
-const MyApp: AppType = ({ Component, pageProps }) => {
+if (typeof window !== 'undefined') { // checks that we are client-side
+  posthog.init(clientEnv.NEXT_PUBLIC_POSTHOG_KEY, {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://us.i.posthog.com',
+    person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
+    loaded: (posthog) => {
+      if (process.env.NODE_ENV === 'development') posthog.debug() // debug mode in development
+    },
+  })
+}
+
+const App: AppType = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
 };
 
-export default MyApp;
+export default App;
